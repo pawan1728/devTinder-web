9ebe290e1409f6b19738fc4f2f0252e58a6f43cc
pawanmishra1728 <pawanthp17@gmail.com>
2025-04-28 00:59:30 +0530

    login apis and setup redux store

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,18 +1,25 @@
 import React, { useState } from "react";
 import axios from "axios";
+import { useDispatch } from "react-redux";
+import { addUser } from "../utils/userSlice";
+import { useNavigate } from "react-router-dom";
+import { BASE_URL } from "../utils/constants";
 
 const Login = () => {
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
   const [emailId, setEmailId] = useState("");
   const [password, setPassword] = useState("");
 
   const handleLogin = async () => {
     try {
       const res = await axios.post(
-        "http://localhost:1234/login",
+        `${BASE_URL}/login`,
         { emailId, password },
         { withCredentials: true }
       );
-      console.log("🚀 ~ handleLogin ~ res:", res);
+    dispatch(addUser(res?.data))
+    navigate("/")
     } catch (error) {
       console.log("🚀 ~ handleLogin ~ error:", error);
     }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,59 @@
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { removeUser } from "../utils/userSlice";
+import { useNavigate } from "react-router-dom";
+
+const Navbar = () => {
+  const user = useSelector((store) => store.user);
+  const dispatch = useDispatch();
+  const navigate = useNavigate()
+  const handleLogout = () =>{
+    dispatch(removeUser())
+    navigate("/login")
+  }
+  return (
+    <div className="navbar bg-base-300 shadow-sm px-5">
+      <div className="flex-1">
+        <a className="btn btn-ghost text-xl">👨‍💻DevTinder</a>
+      </div>
+      {user && (
+        <div className="flex gap-2 items-center">
+          <p className="px-4">Welcome, {user.firstName}</p>
+          <div className="dropdown dropdown-end">
+            <div
+              tabIndex={0}
+              role="button"
+              className="btn btn-ghost btn-circle avatar"
+            >
+              <div className="w-10 rounded-full">
+                <img
+                  alt="Tailwind CSS Navbar component"
+                  src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
+                />
+              </div>
+            </div>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+            >
+              <li>
+                <a className="justify-between">
+                  Profile
+                  <span className="badge">New</span>
+                </a>
+              </li>
+              <li>
+                <a>Settings</a>
+              </li>
+              <li>
+                <a onClick={handleLogout}>Logout</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Navbar;
